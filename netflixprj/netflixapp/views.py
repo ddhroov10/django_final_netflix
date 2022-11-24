51f628afb9ca0c1202f557aa7d031f498d80a569
Dhroov Goel <dhroov19303@iiitd.ac.in>
2022-11-24 23:11:40 +0530

    everything except 3 big movies

--- netflixprj/netflixapp/views.py
+++ netflixprj/netflixapp/views.py
@@ -73,3 +73,18 @@ class MovieDetail(View):
             return render(request, 'moviedetail.html', context)
         except Movie.DoesNotExist:
             return redirect('netflixapp:profile-list')
+
+method_decorator(login_required, name='dispatch')
+class PlayMovie(View):
+    def get(self, request, movie_id, *args, **kwargs):
+        try:
+            movie = Movie.objects.get(uuid=movie_id)
+            movie = movie.video.values()
+            
+            context = {
+                'movie':list(movie)
+            }
+
+            return render(request, 'playmovie.html', context)
+        except Movie.DoesNotExist:
+            return redirect('netflixapp:profile-list')
