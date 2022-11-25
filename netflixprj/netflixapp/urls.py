aa729c2aa074aa78dfd787b261b4ff6fce09064d
Dhroov Goel <dhroov19303@iiitd.ac.in>
2022-11-25 15:26:00 +0530

    everything except 3 big movies, and pycache

--- netflixprj/netflixapp/urls.py
+++ netflixprj/netflixapp/urls.py
@@ -3,11 +3,12 @@ from .views import Home, ProfileList, ProfileCreate, MovieList, MovieDetail, Pla
 
 app_name = 'netflixapp'
 
+# all these url patterns goes to the views.py file and calls the class with the name mentioned with .as_view
 urlpatterns = [
     path('', Home.as_view(), name="Home"),
     path('profiles/', ProfileList.as_view(), name="profile-list"),
     path('profiles/create/', ProfileCreate.as_view(), name="profile-create"),
     path('watch/<str:profile_id>/', MovieList.as_view(), name="movie-list"),
     path('watch/detail/<str:movie_id>/', MovieDetail.as_view(), name="movie-detail"),
-    path('watch/play/<str:movie_id>/', PlayMovie.as_view(), name="play-movie")
+    path('watch/play/<str:movie_id>/', PlayMovie.as_view(), name="play-movie") 
 ]
