7750e77c5808e733d4bc3e6602955c508f288d46
EtashTyagi <etash18@gmail.com>
2022-12-03 19:01:10 +0530

    fixed static_root

--- netflixprj/netflixprj/settings.py
+++ netflixprj/netflixprj/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)cf1@^t=6m7nzm0$4t4j0&y2so*q*g6*mdb^ci_duqi-(u3x5+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.51.25', '127.0.0.1', 'localhost', '0.0.0.0']
+ALLOWED_HOSTS = ['192.168.51.25', '127.0.0.1', 'localhost', '0.0.0.0', "netflixprj.etashtyagi.tk"]
 
 
 # Application definition
@@ -136,7 +136,7 @@ SITE_ID = 1
 AUTH_USER_MODEL = 'netflixapp.CustomUser'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
